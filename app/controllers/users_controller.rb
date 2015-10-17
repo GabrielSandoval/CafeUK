d9f45158b311e7021813d711faf338be5398bc83
gsandoval <gsandoval@gsandoval-VirtualBox.(none)>
2015-10-17 22:37:44 +0800

    Fixed user view on profile privilege and created list user page for the admin.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+
+	def index
+		if can_administer?
+			@users = User.all.order(:id)
+		else
+			redirect_to :root, :notice=>"Unauthorized page"
+		end
+	end
 
 	def show
-		@user = User.find(params[:id])
+		user = User.find(params[:id])
+		if can_administer? || current_user.id == user.id
+			@user = user
+		else
+			redirect_to :root, :notice=>"Unauthorized page"
+		end
 	end
 
 end
