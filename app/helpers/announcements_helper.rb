f4852bebb477b2089a12167e9fb127d13fedb4ca
gsandoval <gsandoval@gsandoval-VirtualBox.(none)>
2015-10-19 01:00:28 +0800

    Fixed announcement display on home page.

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,2 +1,5 @@
 module AnnouncementsHelper
+	def show_date_and_time(date_posted)
+		"#{month_number_to_word(date_posted.month)} #{date_posted.day}, #{date_posted.year} - #{date_posted.hour}:#{format('%2d', date_posted.min)}"		
+	end
 end
